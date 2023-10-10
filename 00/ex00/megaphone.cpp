22456e2f110034a5ed2e21c19bd1abb1680fceaf
Tofaramunashe Mususa <tmususa@lab2r1s6.42abudhabi.ae>
2023-10-10 17:56:52 +0400

    00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tofaramususa <tofaramususa@student.42.f    +#+  +:+       +#+        */
+/*   By: tmususa <tmususa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 22:01:53 by tofaramusus       #+#    #+#             */
-/*   Updated: 2023/09/11 22:08:46 by tofaramusus      ###   ########.fr       */
+/*   Updated: 2023/10/10 17:55:53 by tmususa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,11 @@ int main(int ac, char **argv)
 			std::string input(argv[i]);
 			for(std::string::size_type j = 0; j < input.length(); j++)
 			{
-				input[j] = toupper(input[j]);
+				input[j] = std::toupper(input[j]);
 			}
 			std::cout << input;
 		}
+		std::cout << " " << std::endl;
 		std::cout << std::endl;
 	}
 }
